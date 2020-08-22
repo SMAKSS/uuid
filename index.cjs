24b6496dafa3f2774405570f317392f0a4b5dd95
MAKSS <kazemi914@live.com>
2020-08-22 21:33:48 +0430

    Package got updated!

--- index.cjs
+++ index.cjs
@@ -1,11 +1,11 @@
-'use strict'
+'use strict';
 function uuid() {
   return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, function (c) {
     return (
       c ^
       (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-    ).toString(16)
-  })
+    ).toString(16);
+  });
 }
 
-module.exports = uuid
+module.exports = uuid;
