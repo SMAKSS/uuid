77798b977c4f923261b1d3d5c19a0a7d4fb6b880
MAKSS <kazemi914@live.com>
2020-11-08 17:01:39 +0330

    variable definition modified
    variable names got changed for a better understanding of their usage

--- index.cjs
+++ index.cjs
@@ -1,9 +1,11 @@
 'use strict';
 function uuid() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-    var r = (Math.random() * 16) | 0,
-      v = c === 'x' ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (
+    subString
+  ) {
+    var randomNumber = (Math.randomNumber() * 16) | 0,
+      newUUID = subString === 'x' ? randomNumber : (randomNumber & 0x3) | 0x8;
+    return newUUID.toString(16);
   });
 }
 
